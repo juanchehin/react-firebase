6f002bb005d54ff223edc7c08ce68033df658293
Your Name <you@example.com>
2022-02-01 17:30:04 -0300

    Temperatura

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,12 @@
 import React from 'react';
 import './App.css';
-import Listado from './components/listado';
+import Temperatura from './components/temperatura';
 
 function App() {
   return (
     <div className="App">
       <h1>Titulo de nuestro primer proyecto</h1>
-      <Listado></Listado>
+      <Temperatura></Temperatura>
     </div>
   );
 }
---
+++ src/components/temperatura.jsx
@@ -0,0 +1,29 @@
+import React,{useState} from 'react'
+
+const Temperatura = () => {
+
+    const [temperatura,setTemperatura] = useState(19)
+
+    const Subir = () =>{
+        setTemperatura(temperatura + 1)
+    }
+
+    const Bajar = () =>{
+        setTemperatura(temperatura - 1)
+    }
+
+    return (
+        <div>
+            <h2>La temperatura es : {temperatura}</h2>
+            <p>
+                {
+                    temperatura > 21 ? 'Hace calor' : 'Hace frio'
+                }
+            </p>
+            <button onClick={Subir}>Aumentar</button>
+            <button onClick={Bajar}>Reducir</button>
+        </div>
+    )
+}
+
+export default Temperatura
