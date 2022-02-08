1bf7a443c6da5e10af560af9d4d685006d6e0d96
Your Name <you@example.com>
2022-02-08 16:00:49 -0300

    estructura basica de props

---
+++ props/src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import Bienvenida from './components/Bienvenida';
+
+function App() {
+  return (
+    <div>
+      <h1>Propiedades de los componentes</h1>
+      <Bienvenida/>
+    </div>
+  );
+}
+
+export default App;
