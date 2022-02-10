ab6b9c55edce5b66c92925ee018c21d4b15ae81a
Your Name <you@example.com>
2022-02-10 17:44:46 -0300

    useeffect

--- router/src/components/Base.jsx
+++ router/src/components/Base.jsx
@@ -1,9 +1,19 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
 
 const Base = () => {
+    const [nombre, setNombre] = useState('Pedro')
+
+    useEffect ( () => {
+        setTimeout( () =>{
+            setNombre('Manuel')
+        },2000)
+    })
+
+
   return (
     <div>
         <h1>Pagina de base</h1>
+        {nombre}
     </div>
   );
 }
