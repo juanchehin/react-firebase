cbb6345cbfaae8020626e4639fbd88daf53f1a07
Your Name <you@example.com>
2022-02-11 16:37:49 -0300

    components y rutas

--- firebase-auth/src/App.jsx
+++ firebase-auth/src/App.jsx
@@ -1,10 +1,24 @@
-import react from 'react'
+import React from 'react';
+import Inicio from './components/Inicio';
+import Login from './components/Login';
+import Admin from './components/Admin';
+
+import {
+  BrowserRouter,
+  Routes ,
+  Route,
+  Link
+} from 'react-router-dom'
 
 function App() {
   return (
-    <div className="App">
-      
-    </div>
+    <BrowserRouter>
+      <Routes>
+          <Route path='inicio' element={<Inicio />} />
+          <Route path='login' element={<Login />} />
+          <Route path='admin' element={<Admin />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
